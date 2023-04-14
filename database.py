5f42074738418a3728764c568a1f91a8fdad1d98
ecandrade <98795946+ecandrade@users.noreply.github.com>
2023-04-14 04:51:52 +0000

    asdfa

---
+++ database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://vq5e8wkq2merroa1fnv0:pscale_pw_BUpGSDHvLooAVyF7HSGsZSt1ffL2l1Z2ipDWNS4jUNc@aws.connect.psdb.cloud/eduflaskapp?charset=utf8mb4")
